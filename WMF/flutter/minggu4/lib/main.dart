3577237a2787dba69f56e381ebc83f91f9e05207
DIVYANISA\Novianto Hadi <novianto_hadi@polije.ac.id>
2021-10-08 09:25:48 +0700

    Minggu4 Sukses

--- WMF/flutter/minggu4/lib/main.dart
+++ WMF/flutter/minggu4/lib/main.dart
@@ -4,14 +4,16 @@ import './tab_dua.dart' as tabdua;
 import './tab_tiga.dart' as tabtiga;
 
 void main() {
-  runApp(const MaterialApp(
-    title: "tabbar image",
-    home: PageOne(),
-  ));
+  runApp(
+    const MaterialApp(
+      title: "Tabbar_Image",
+      home: PageOne(),
+    )
+  );
 }
 
 class PageOne extends StatefulWidget {
-  const PageOne({Key? key}) : super(key: key);
+  const PageOne({ Key? key }) : super(key: key);
 
   @override
   _PageOneState createState() => _PageOneState();
@@ -19,6 +21,7 @@ class PageOne extends StatefulWidget {
 
 class _PageOneState extends State<PageOne> with SingleTickerProviderStateMixin {
   late TabController controller;
+
   @override
   void initState() {
     controller = TabController(length: 3, vsync: this);
@@ -27,7 +30,7 @@ class _PageOneState extends State<PageOne> with SingleTickerProviderStateMixin {
 
   @override
   void dispose() {
-    controller.dispose(); // TODO: implement dispose
+    controller.dispose();
     super.dispose();
   }
 
@@ -38,25 +41,25 @@ class _PageOneState extends State<PageOne> with SingleTickerProviderStateMixin {
         backgroundColor: Colors.black,
         title: const Text("Daftar Halaman"),
         bottom: TabBar(
-          controller: controller,
-          tabs: const <Widget>[
-            Tab(
-              icon: Icon(Icons.home),
-            ),
-            Tab(
-              icon: Icon(Icons.favorite),
-            ),
-            Tab(
-              icon: Icon(Icons.place),
-            )
-          ],
+        controller: controller,
+        tabs: const <Widget>[
+          Tab(
+            icon: Icon(Icons.home),
+          ),
+          Tab(
+            icon: Icon(Icons.favorite),
+          ),
+          Tab(
+            icon: Icon(Icons.place),
+          ),
+        ],
         ),
       ),
-      body: TabBarView(controller: controller, children: const <Widget>[
+      body: TabBarView(controller: controller,children: const <Widget> [
         tabsatu.tabSatu(),
         tabdua.tabDua(),
         tabtiga.tabTiga(),
-      ]),
+      ],),
     );
   }
 }
---
+++ WMF/flutter/minggu4/lib/tab_dua.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class tabDua extends StatelessWidget {
+  const tabDua({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(20.0),
+              ),
+              Text(
+                "Tab Dua",
+                style: TextStyle(fontSize: 30.0),
+              ),
+              Padding(
+                padding: EdgeInsets.all(20.0),
+              ),
+              Image.asset(
+                "img/fav.png",
+                width: 200.0,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
